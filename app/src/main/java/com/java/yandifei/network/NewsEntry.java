992ea83582c6bd640518fb0583a44dbcf1364392
kxz-Manjaro <15068701650@163.com>
2020-09-05 20:36:57 +0800

    good java

--- app/src/main/java/com/java/yandifei/network/NewsEntry.java
+++ app/src/main/java/com/java/yandifei/network/NewsEntry.java
@@ -1,5 +1,8 @@
 package com.java.yandifei.network;
 
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.widget.ArrayAdapter;
@@ -33,6 +36,7 @@ public class NewsEntry implements Serializable {
     public String content;
     public String type;
     public List<Author> authors = new ArrayList<Author>();
+    private static FragmentActivity activity;
     private static String newsEntryUrl = "https://covid-dashboard.aminer.cn/api/events/list";
 
 
@@ -46,6 +50,24 @@ public class NewsEntry implements Serializable {
                 ", authors: " + authors + ")";
     }
 
+    public static void setActivity(FragmentActivity activity) {
+        NewsEntry.activity = activity;
+    }
+
+    public static void saveNewsEntry(String tableName, NewsEntry news) {
+        MyApplication globalVariables = (MyApplication)activity.getApplication();
+        SharedPreferences storage = activity.getSharedPreferences(tableName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = storage.edit();
+        editor.putString(news.id, JSON.toJSONString(news));
+        editor.commit();
+    }
+
+    public static boolean newsIsIntable(String tableName, String newsId) {
+        MyApplication globalVariables = (MyApplication)activity.getApplication();
+        SharedPreferences storage = activity.getSharedPreferences(tableName, Context.MODE_PRIVATE);
+        return storage.contains(newsId);
+    }
+
     public static void getNewsList(CharSequence tag, int page, int entryNumPerPage, final List<NewsEntry> newsList) {
         OkHttpClient client = new OkHttpClient();
         String url = newsEntryUrl + "?type=" + tag + "&page=" + page +
@@ -53,25 +75,30 @@ public class NewsEntry implements Serializable {
         final Request request = new Request.Builder().get().url(url).build();
         Response response = null;
         try {
+            MyApplication globalVariables = (MyApplication) activity.getApplication();
             response = client.newCall(request).execute();
             if (response.code() == 200) {
                 final String result = response.body().string();
-                JSONArray dataArray = ((Map<String, JSONArray>)JSON.parse(result)).get("data");
+                JSONArray dataArray = ((Map<String, JSONArray>) JSON.parse(result)).get("data");
                 for (int i = 0; i < dataArray.size(); ++i) {
                     NewsEntry entry = JSON.parseObject(dataArray.getString(i), NewsEntry.class);
                     if (entry.source.equals("")) {
                         if (entry.authors.size() == 0) entry.source = "Unknown";
                         else {
                             for (Author author : entry.authors) entry.source += author.name + ", ";
-                            entry.source = entry.source.substring(0, entry.source.length()-3);
+                            entry.source = entry.source.substring(0, entry.source.length() - 3);
                         }
                     }
-                    // System.out.println(entry);
                     newsList.add(entry);
+                    if (!newsIsIntable(globalVariables.newsListTableName, entry.id))
+                        saveNewsEntry(globalVariables.newsListTableName, entry);
                 }
             }
         } catch (IOException e) {
             System.out.println("mylog: " + e);
+        } catch (Exception e) {
+            // no network access
+
         } finally {
             if (response != null) response.body().close();
         }
