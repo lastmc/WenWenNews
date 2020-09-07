3022b63028514f3795da9fc7df8d04e2c04c9435
LastMC <last_mc@126.com>
2020-09-07 11:22:03 +0800

    add scholar activity

--- app/src/main/java/com/java/yandifei/ui/corona/CoronaFragment.java
+++ app/src/main/java/com/java/yandifei/ui/corona/CoronaFragment.java
@@ -7,8 +7,9 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
 import com.java.yandifei.R;
 
 import java.util.ArrayList;
@@ -16,12 +17,8 @@ import java.util.List;
 
 public class CoronaFragment extends Fragment {
 
-    private CoronaViewModel coronaViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        coronaViewModel =
-                ViewModelProviders.of(this).get(CoronaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_corona, container, false);
         List<CharSequence> list = new ArrayList<>();
         list.add(getString(R.string.tab_corona_data));
@@ -29,8 +26,39 @@ public class CoronaFragment extends Fragment {
         list.add(getString(R.string.tab_corona_news));
         list.add(getString(R.string.tab_corona_scholar));
 
-        //bindTabWithViewPager(view,list);
+        bindTabWithViewPager(root,list);
         setHasOptionsMenu(true);
         return root;
+    }
+
+    private void bindTabWithViewPager(View view,final List<CharSequence> list){
+        final TabLayout tabLayout = view.findViewById(R.id.corona_tabs);
+        final ViewPager2 viewPager2 = view.findViewById(R.id.corona_container);
+        final CoronaTabViewPagerAdapter adapter = new CoronaTabViewPagerAdapter(list,getActivity());
+        for(CharSequence c:list)
+            tabLayout.addTab(tabLayout.newTab().setText(c));
+
+        viewPager2.setAdapter(adapter);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = list.indexOf(tab.getText());
+                viewPager2.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+        ViewPager2.OnPageChangeCallback callback = new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+                super.onPageSelected(position);
+            }
+        };
+        viewPager2.registerOnPageChangeCallback(callback);
     }
 }
